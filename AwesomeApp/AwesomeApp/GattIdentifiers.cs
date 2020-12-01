447c392e0453d09cff732ab38ee0b6b264fda269
kassymkhan bekbolatov <kbekbolatov@biointeractivetech.com>
2020-12-01 15:16:55 -0800

    modified info.plist

--- AwesomeApp/AwesomeApp/GattIdentifiers.cs
+++ AwesomeApp/AwesomeApp/GattIdentifiers.cs
@@ -9,7 +9,7 @@ namespace AwesomeApp
         public static Guid UartGattServiceId = Guid.Parse("0003CDD0-0000-1000-8000-00805F9B0131");
         public static Guid UartGattCharacteristicReceivedId = Guid.Parse("0003CDD1-0000-1000-8000-00805F9B0131");
         public static Guid UartGattCharacteristicSendId = Guid.Parse("0003CDD2-0000-1000-8000-00805F9B0131");
-        public static Guid SpecialNotificationDescriptorId = Guid.Parse("92495146-C796-44BE-8B50-2FBAEAB1A4E8"); 
+        public static Guid SpecialNotificationDescriptorId = Guid.Parse("0003CDD0-0000-1000-8000-00805F9B0131"); 
         
         
         
